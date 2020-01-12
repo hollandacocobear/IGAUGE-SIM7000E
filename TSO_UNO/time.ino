c0c6cf5bdc291a93a00b22a3ede9d256e0d63496
Hollanda Arief Kusuma <hollandacocobear@gmail.com>
2020-01-13 01:55:37 +0700

    Update time.ino
    penggunaan char array untuk mendapatkan nilai waktu

--- TSO_UNO/time.ino
+++ TSO_UNO/time.ino
@@ -10,57 +10,80 @@ void NTPtime() {
 
 void waktu() {
   //bersihkan variabel
+  hapus();
   tahun = '0'; bulan = '0'; hari = '0'; jam = '0'; menit = '0'; detik = '0';
 
   //AMBIL WAKTU DARI PROVIDER
-//  Serial.println("ambil waktu");
-  SIM7000.println(F("AT+CCLK?"));
+  //  Serial.println("ambil waktu");
+  Serial.flush();
   SIM7000.flush();
+  SIM7000.println(F("AT+CCLK?"));
   bacaserial(500);
   delay(300);
 
+  //debug
+  //  Serial.println(F("hasil dari cclk="));
+  //  Serial.println(kalimat);
+
   //PISAHKAN DATA WAKTU
   //tahun
-  indeks2 = kalimat.indexOf("/", indeks1 + 1);
-//  Serial.println(indeks2);
-  tahun = kalimat.substring(indeks2-2, indeks2).toInt();
+  strcat(json,kalimat.c_str());
+  char *p = strstr(json,"\"");
+//  Serial.println(p);
+//  Serial.println(int(p-json));
+  kata[0]=json[int(p-json)+1];
+  kata[1]=json[int(p-json)+2];
+  tahun=atoi(kata);
 //  Serial.println(tahun);
-
+  
   //bulan
-  indeks1 = kalimat.indexOf("/", indeks2 + 1);
-  bulan = kalimat.substring(indeks2 + 1, indeks1).toInt();
+  kata[0]=json[int(p-json)+4];
+  kata[1]=json[int(p-json)+5];
+  bulan = atoi(kata);
+//  Serial.println(bulan);
 
   //hari
-  indeks2 = kalimat.indexOf(",", indeks1 + 1);
-  hari = kalimat.substring(indeks1 + 1, indeks2).toInt();
+  kata[0]=json[int(p-json)+7];
+  kata[1]=json[int(p-json)+8];
+  hari = atoi(kata);
+//  Serial.println(hari);
 
   //jam
-  indeks1 = kalimat.indexOf(":", indeks2 + 1);
-  jam = kalimat.substring(indeks2 + 1, indeks1).toInt();
+  kata[0]=json[int(p-json)+10];
+  kata[1]=json[int(p-json)+11];
+  jam = atoi(kata);
+//  Serial.println(jam);
 
   //menit
-  indeks2 = kalimat.indexOf(":", indeks1 + 1);
-  menit = kalimat.substring(indeks1 + 1, indeks2).toInt();
+  kata[0]=json[int(p-json)+13];
+  kata[1]=json[int(p-json)+14];
+  menit = atoi(kata);
+//  Serial.println(menit);
 
   //detik
-  indeks1 = kalimat.indexOf("+", indeks2 + 1);
-  detik = kalimat.substring(indeks2 + 1, indeks1).toInt();
+  kata[0]=json[int(p-json)+16];
+  kata[1]=json[int(p-json)+17];
+  detik = atoi(kata);
+//  Serial.println(detik);
+  
+  setTime(jam, menit, detik, hari, bulan, tahun);
 
   //Keluarkan tanggal di serial monitor
+  hapus();
   Serial.print(F("YYYY/MM/DD HH:MM:SS="));
-  Serial.print("20");
-  Serial.print(tahun);
-  Serial.write("/");
-  Serial.print(bulan);
-  Serial.write("/");
-  Serial.print(hari);
-  Serial.write(" ");
-  Serial.print(int2digits(jam));
-  Serial.write(":");
-  Serial.print(int2digits(menit));
-  Serial.write(":");
-  Serial.println(int2digits(detik));
-  setTime(jam, menit, detik, hari, bulan, tahun);
+  strcat(tanggal, "20");
+  strcat(tanggal, itoa(tahun,json,10));
+  strcat(tanggal, "/");
+  strcat(tanggal, int2digits(bulan).c_str());
+  strcat(tanggal, "/");
+  strcat(tanggal, int2digits(hari).c_str());
+  strcat(tanggal, " ");
+  strcat(tanggal, int2digits(jam).c_str());
+  strcat(tanggal, ":");
+  strcat(tanggal, int2digits(menit).c_str());
+  strcat(tanggal, ":");
+  strcat(tanggal, int2digits(detik).c_str());
+  Serial.println(tanggal);
 }
 
 void waktuTunggu() {
