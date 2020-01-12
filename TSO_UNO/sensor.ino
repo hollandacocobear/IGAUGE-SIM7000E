6127636f330fdb3ac3831220098f28dbb0d0c390
Hollanda Arief Kusuma <hollandacocobear@gmail.com>
2020-01-13 01:56:40 +0700

    Update sensor.ino
    penggunaan operasi str c# (array) untuk menuliskan JSON

--- TSO_UNO/sensor.ino
+++ TSO_UNO/sensor.ino
@@ -2,7 +2,7 @@ void sensor() {
   unsigned long nilai = 0;
   float pressVolt = 0;
   tekanan = 0;
-  
+
   for (indeks = 0; indeks < burst; indeks++) {
     int pressADC = analogRead(pressurePin);
     nilai += pressADC;
@@ -12,53 +12,30 @@ void sensor() {
   tekanan = (3.00 * pressVolt - 1.5) - offset; // satuan bar
 }
 
-void dataJSON(){
-  //http://www.mantisid.id/api/product/osh_data_c.php?= {"Data":"003,20190315213555,2456,02356,02201,01233,05,15"}
+void dataJSON() {
+  //http://www.mantisid.id/project/osh_2019/api/product/osh_data_c.php?{"Data":"003,20190315213555,2456,02356,02201,01233,05,15"}
   // {"Data":"id unit,yyyymmddhhmmss,tekanan(5 digit),kelembaban(5 digit),suhu(5 digit),volt (5 digit), burst (2 digit), interval (2 digit)"}
-  json="";
-  json  = "{\"Data\":\"";
-  json.concat(String(ID));
-  json.concat(String(",20"));
-  json.concat(String(tahun));
-  if (bulan < 10) {
-    json += "0" + String(bulan);
-  }
-  if (bulan >= 10) {
-    json += String(bulan);
-  }
-  if (hari < 10) {
-    json += "0" + String(hari);
-  }
-  if (hari >= 10) {
-    json += String(hari);
-  }
-  if (jam < 10) {
-    json += "0" + String(jam);
-  }
-  if (jam >= 10) {
-    json.concat(String(jam));
-  }
-  if (menit < 10) {
-    json += "0" + String(menit);
-  }
-  if (menit >= 10) {
-    json.concat(String(menit));
-  }
-  if (detik < 10) {
-    json += "0" + String(detik);
-  }
-  if (detik >= 10) {
-    json.concat(String(detik));
-  }
+  hapus();
+  strcat(json, "{\"Data\":\"");
 
-  json.concat(",");
-  json.concat(String(tekanan,2));
-  json.concat(",-9999,-9999,-9999,");
-  json.concat(String(burst));
-  json.concat(",");
-  json.concat(String(interval));
-  json.concat("\"}");
+  //ID
+  strcat(json, ID);
+  strcat(json, ",");
+  //  Serial.println(data);
 
+  //tanggal
+  strcat(json, tanggal);
+  strcat(json, ",");
+  //  Serial.println(data);
+  kalimat = String(tekanan, 2);
+  strcat(json, kalimat.c_str());
+  strcat(json, ",-9999,-9999,-9999,");
+//  Serial.println(json);
+
+  strcat(json,String(burst).c_str());
+  strcat(json,",");
+  strcat(json,String(interval).c_str());
+  strcat(json,"\"}");
   Serial.println(json);
   Serial.flush();
 }
